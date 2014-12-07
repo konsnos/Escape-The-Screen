d159be212d1acc80f2a1704546845d774db6275e
Konstantinos Egkarchos <konsnosl@gmail.com>
2014-12-07 20:53:45 +0200

    Initial commit.

---
+++ EscapeTheScreen/Assets/Scripts/BootScreenText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EscapeTheScreen
+{
+    public class BootScreenText : MonoBehaviour
+    {
+        [SerializeField]
+        private string text;
+        private int caretPos;
+        private bool ready;
+        public bool Ready { get { return ready; } }
+
+        // Use this for initialization
+        void Start()
+        {
+            ready = false;
+            caretPos = 0;
+            StartCoroutine("typeText");
+        }
+
+        // Update is called once per frame
+        IEnumerator typeText()
+        {
+            yield return null;
+        }
+    }
+}
